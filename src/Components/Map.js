38b4919f7a597f56660d64bfd77bf1e2a040ba2b
Amir Abbas <amirabbasit786@gmail.com>
2021-06-20 02:22:27 +0500

    added widgets and design

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -1,15 +1,56 @@
-import React, { useCallback, useEffect, useRef } from 'react'
+import React, { useCallback, useEffect, useRef, useState } from 'react'
 import { loadModules } from 'esri-loader';
+import Widgets from './Widgets';
 const Map = () => {
 
+    const [view, setView] = useState(null);
+    useEffect(() => {
+        console.log("View", view);
+    }, [view])
     const mapRef = useRef();
 
+    // Widgets Refs...
+    const searchRef = useRef();
+    const daylightRef = useRef();
+    const basemapRef = useRef();
+    const distanceRef = useRef();
+    const areaRef = useRef();
+    const layersRef = useRef();
+    const legendsRef = useRef();
+    const fullScreenRef = useRef();
+
+    //
     useEffect(() => {
         loadModules([
             "esri/config",
             "esri/Map",
-            "esri/views/SceneView",], { css: true })
-            .then(([esriConfig, Map, SceneView]) => {
+            "esri/views/SceneView",
+            "esri/widgets/Search",
+            "esri/widgets/DirectLineMeasurement3D",
+            "esri/widgets/AreaMeasurement3D",
+            "esri/widgets/BasemapGallery",
+            "esri/widgets/Daylight",
+            "esri/widgets/LayerList",
+            "esri/widgets/Legend",
+            "esri/widgets/Fullscreen",
+            "esri/webscene/Slide"
+        ], { css: true })
+            .then(([
+                esriConfig,
+                Map,
+                SceneView,
+                Search,
+                DirectLineMeasurement3D,
+                AreaMeasurement3D,
+
+                BasemapGallery,
+                Daylight,
+
+                LayerList,
+                Legend,
+                Fullscreen,
+                Slide
+            ]) => {
                 esriConfig.apiKey = "AAPK9c7f8c1db2274a55ac08483ae7df46a7AmG6j3sj2BPjdDShFCXB9gm4R2ijnCHLPww42eJSwQF46AbjrD1rR6GJS98-uRqs";
 
                 const map = new Map({
@@ -30,13 +71,181 @@ const Map = () => {
                     }
                 });
 
+                // Widget 1 Search Widget
+
+                var searchWidget = new Search({
+                    container: searchRef.current,
+                    view: view
+                });
+
+                // // Add the search widget to the top right corner of the view
+                // view.ui.add(searchWidget, {
+                //     position: "top-right"
+                // });
+
+                // Widget 2 Distance Widget
+                var distanceWidget = new DirectLineMeasurement3D({
+                    container: distanceRef.current,
+                    view: view
+                });
+
+                //   // skip the initial 'new measurement' button
+                // distanceWidget.viewModel.start();
+                // view.ui.add(distanceWidget, "top-right");
+
+                // Widget 3 Area Widget
+                var areaWidget = new AreaMeasurement3D({
+                    container: areaRef.current,
+                    view: view
+                });
+
+                // // skip the initial 'new measurement' button
+                // areaWidget.viewModel.start();
+                // view.ui.add(areaWidget, "top-right");
+
+                // Widget 4 Layers Widget
+
+                var basemapGallery = new BasemapGallery({
+                    container: basemapRef.current,
+                    view: view
+                });
+
+                // Add the widget to the top-right corner of the view
+                // view.ui.add(basemapGallery, {
+                //     position: "top-right"
+                // });
+
+                // Widget 5 Layers Widget
+
+                const daylightWidget = new Daylight({
+                    container: daylightRef.current,
+                    view: view,
+                    // plays the animation twice as fast than the default one
+                    playSpeedMultiplier: 2,
+                    // disable the timezone selection button
+                    visibleElements: {
+                        timezone: false
+                    }
+                });
+                // view.ui.add(daylightWidget, "top-right");
+
+                // Widget 6 Legends 
+                // var featureLayer = view.layers.getItemAt(0);
+
+                // var legend = new Legend({
+                //     view: view,
+                //     // layerInfos: [
+                //     //     {
+                //     //         layer: featureLayer,
+                //     //         title: "NY Educational Attainment"
+                //     //     }
+                //     // ]
+                // });
+                // view.ui.add(legend, "top-right");
+
+                // Widget 7 Layers Widget
+
+                var layerList = new LayerList({
+                    container: layersRef.current,
+                    view: view,
+                    listItemCreatedFunction: function (event) {
+                        const item = event.item;
+                        if (item.layer.type != "group") {
+                            // don't show legend twice
+                            item.panel = {
+                                content: "legend",
+                                open: true
+                            };
+                        }
+                    }
+                });
+
+                // // Add widget to the top right corner of the view
+                // view.ui.add(layerList, "top-right");
+
+                // Widget 8 Legends..
+
+                var legend = new Legend({
+                    container: legendsRef.current,
+                    view: view,
+                    style: "card" // other styles include 'classic'
+                });
+                // view.ui.add(legend, "bottom-left");
+
+                // Widget 9 FullScreen
+                var fullscreen = new Fullscreen({
+                    // container: fullScreenRef.current,
+                    view: view,
+                    // id: "full-widget",
+                    // declaredClass: "full-screen-custom"
+                });
+                view.ui.add(fullscreen, "bottom-right");
+
+
+                ///// Slides Code...
+                // view.ui.add(["createSlideDiv", "slidesDiv"], "top-right");
+
+                function createSlideUI(slide, placement) {
+                    var slideElement = document.createElement("div");
+                    slideElement.id = slide.id;
+                    slideElement.classList.add("slide");
+                    var slidesDiv = document.getElementById("slidesDiv");
+                    if (placement === "first") {
+                        slidesDiv.insertBefore(slideElement, slidesDiv.firstChild);
+                    } else {
+                        slidesDiv.appendChild(slideElement);
+                    }
+
+                    var title = document.createElement("div");
+                    title.innerText = slide.title.text;
+                    slideElement.appendChild(title);
+
+                    var img = new Image();
+                    img.src = slide.thumbnail.url;
+                    img.title = slide.title.text;
+                    slideElement.appendChild(img);
+                    slideElement.addEventListener("click", function () {
+                        var slides = document.querySelectorAll(".slide");
+                        Array.from(slides).forEach(function (node) {
+                            node.classList.remove("active");
+                        });
+                        slideElement.classList.add("active");
+                        slide.applyTo(view);
+                    });
+                }
+
+                document.getElementById("slidesDiv").style.visibility = "visible";
+                var slides = view.presentation?.slides;
+                slides?.forEach(createSlideUI);
+                document.getElementById("createSlideButton").addEventListener("click", function () {
+                    Slide.createFrom(view).then(function (slide) {
+                        slide.title.text = document.getElementById("createSlideTitleInput").value;
+                        view.presentation?.slides?.add(slide);
+
+                        createSlideUI(slide, "first");
+                    });
+                });
+
             })
     }, [])
 
+    useEffect(() => {
+        console.log("View:", view)
+    }, [view])
+
     return (
         <div>
+            <Widgets
+                searchRef={searchRef}
+                basemapRef={basemapRef}
+                daylightRef={daylightRef}
+                distanceRef={distanceRef}
+                areaRef={areaRef}
+                layersRef={layersRef}
+                legendsRef={legendsRef}
+                fullScreenRef={fullScreenRef}
+            />
             <div style={{ height: "100vh", width: "100vw" }} className="topo-map" ref={mapRef}>
-
             </div>
         </div>
     )
---
+++ src/Components/Widgets.js
@@ -0,0 +1,160 @@
+import React, { useState, useRef, useEffect } from 'react'
+
+const Widgets = ({ searchRef, basemapRef, daylightRef, distanceRef, areaRef, layersRef, legendsRef, fullScreenRef }) => {
+
+    const [selectedWidget, setSelectedWidget] = useState("")
+    const [fullScreen, setFullScreen] = useState(false);
+    const [activeSidebar, setActiveSidebar] = useState(false);
+    const changeWidget = (widget) => {
+        if (widget === selectedWidget)
+            return setSelectedWidget("")
+        else {
+            switch (widget) {
+                case "distance":
+                    return setSelectedWidget("distance");
+                case "area":
+                    return setSelectedWidget("area");
+                case "daylight":
+                    return setSelectedWidget("daylight");
+                case "basemap":
+                    return setSelectedWidget("basemap");
+                case "slides":
+                    return setSelectedWidget("slides");
+                case "layers":
+                    return setSelectedWidget("layers");
+                case "legends":
+                    return setSelectedWidget("legends");
+                default:
+                    return;
+            }
+        }
+
+    }
+
+    // const [widget, setWidget] = useState(null);
+
+    // useEffect(() => {
+    //     console.log("huhuhuh", daylightRef, basemapRef)
+    //     switch (selectedWidget) {
+    //         case "daylight":
+    //             return setWidget(widget => daylightRef.current);
+    //         case "basemap":
+    //             return setWidget(widget => basemapRef.current);
+    //     }
+    //     // widget.current = daylightRef;
+    // }, [selectedWidget])
+
+    // useEffect(() => {
+    //     console.log(daylightRef, basemapRef)
+    // }, [daylightRef, basemapRef])
+    // const getWidget = () => { }
+    return (
+        <div>
+            <div className={`top-bar ${fullScreen ? "hide-top" : ""}`}>
+                <p>Asset Management</p>
+            </div>
+
+            <div className="sidebar">
+                <div>
+                    <div className={`sidebar-content ${!activeSidebar ? "inactive-sidebar" : ""}`}>
+                        <div className="filter-icon" onClick={() => setActiveSidebar(!activeSidebar)}>
+                            <div className="filter-icon-wrapper">
+                                <img src="/assets/chart-pie.svg" alt="not found" />
+                            </div>
+                            <div className="filter-icon-wrapper">
+                                <img src="/assets/table.svg" alt="not found" />
+                            </div>
+                        </div>
+                        <div className={`content`}>
+                            <div className={`content-header ${!activeSidebar ? "inactive-header" : ""}`}>
+                                <p>Charts</p>
+                                <div className="close-icon" onClick={() => setActiveSidebar(false)}>
+                                    <img src="/assets/metro-cross.svg" alt="not found" />
+                                </div>
+                            </div>
+                            <div className={`content-body`}>
+                                <div className="card"></div>
+                                <div className="card"></div>
+                                <div className="card"></div>
+                                <div className="card"></div>
+                                <div className="card"></div>
+                                <div className="card"></div>
+                                <div className="card"></div>
+                                <div className="card"></div>
+                                <div className="card"></div>
+                                <div className="card"></div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+            <div className={`widget-section ${selectedWidget ? "active" : "inactive"}`} >
+                <div className={`${selectedWidget == "area" ? "active esri-area-measurement-3d__container" : "inactive"}`} ref={areaRef}>
+                </div>
+                <div className={`${selectedWidget == "distance" ? "active esri-direct-line-measurement-3d__container" : "inactive"}`} ref={distanceRef}>
+                </div>
+                <div className={`${selectedWidget == "daylight" ? "active esri-daylight esri-widget" : "inactive"}`} ref={daylightRef}>
+                </div>
+                <div className={`${selectedWidget == "basemap" ? "active" : "inactive"}`} ref={basemapRef}>
+                </div>
+                <div className={`${selectedWidget == "layers" ? "active" : "inactive"}`} ref={layersRef}>
+                </div>
+                <div className={`${selectedWidget == "legends" ? "active" : "inactive"}`} ref={legendsRef}>
+                </div>
+                <div className={`${selectedWidget == "slides" ? "active" : "inactive"}`} >
+                    <div id="createSlideDiv" class="esri-widget">
+                        New slide: <input type="text" id="createSlideTitleInput" size="10" />
+                        <button id="createSlideButton">Create</button>
+                    </div>
+                    <div id="slidesDiv" class="esri-widget"></div>
+                </div>
+            </div>
+
+            <div className={`bottom-bar ${fullScreen ? "hide-bottom" : ""}`} >
+                <div className="search-bar" ref={searchRef}>
+                </div>
+
+                <div className="widgets-container">
+                    <div className="widget-icon">
+                        <span className={selectedWidget == "distance" ? "selected" : ""} onClick={() => changeWidget("distance")}>
+                            <img src={'/assets/ruler-horizontal.svg'} alt="notFound" />
+                        </span>
+                        <span className={selectedWidget == "area" ? "selected" : ""} onClick={() => changeWidget("area")}>
+                            <img src={'/assets/ruler-combined.svg'} alt="notFound" />
+                        </span>
+                        <span className={selectedWidget == "basemap" ? "selected" : ""} onClick={() => changeWidget("basemap")}>
+                            <img src={'/assets/metro-map.svg'} alt="notFound" />
+                        </span>
+                        <span className={selectedWidget == "slides" ? "selected" : ""} onClick={() => changeWidget("slides")}>
+                            <img src={'/assets/slideshow.svg'} alt="notFound" />
+                        </span>
+                        <span className={selectedWidget == "daylight" ? "selected" : ""} onClick={() => changeWidget("daylight")}>
+                            <img src={'/assets/lightbulb.svg'} alt="notFound" />
+                        </span>
+                        <span className={selectedWidget == "layers" ? "selected" : ""} onClick={() => changeWidget("layers")}>
+                            <img src={'/assets/layer-group.svg'} alt="notFound" />
+                        </span>
+                        <span className={selectedWidget == "legends" ? "selected" : ""} onClick={() => changeWidget("legends")}>
+                            <img src={'/assets/list.svg'} alt="notFound" />
+                        </span>
+                    </div>
+                </div>
+
+                {/* <span className="full-screen-icon" onClick={() => setFullScreen(!fullScreen)}>
+                    <img src={'/assets/fullscreen_button.svg'} alt="notFound" />
+                </span> */}
+            </div>
+            <span
+                className="full-screen-icon"
+                //  onClick={() => setFullScreen(!fullScreen)}
+                ref={fullScreenRef}
+            >
+                {/* <img src={'/assets/fullscreen_button.svg'} alt="notFound" /> */}
+            </span>
+        </div>
+    )
+}
+
+export default Widgets
